7a903e1907f0e1e957aad3c3bb04924c23e1c64a
Manix <victor.moncada001@gmail.com>
2025-05-13 12:57:21 -0400

    manejo de listas y datos

---
+++ auxiliares/listas.py
@@ -0,0 +1,10 @@
+def fn_actualizar_listas(l_nombre,l_precio,l_stock):
+    lnombre = l_nombre
+    lprecio = l_precio
+    lstock = l_stock
+    return lnombre,lprecio,lstock
+
+
+lnombre = ["Plumon", "Borrador", "Pizarra"]
+lprecio = [1280.0, 3500.0, 13500.0]
+lstock = [20,8, 10]
--- iec-170.py
+++ iec-170.py
@@ -1,4 +1,7 @@
-import version.version
+from version.version import version
+from auxiliares.listas import lnombre,lprecio,lstock,fn_actualizar_listas
+from auxiliares.valdiacion_numero import fn_get_num_valido
+
 #Sistema de gestion de inventario para una tienda
 #autor: Manuel Sanchez
 """
@@ -22,41 +25,6 @@ PATCH: (parche o revisión): Se incrementa, cuando se corrigen errores en el sis
 #                  agregar control de keyboardInterrupt v2.0.1
 #       
 
-
-def fn_get_num_valido(mensaje):
-    """
-    funcion para validar el ingreso de un valor numérico.
-    No termina hasta que el valor ingresado es válido
-    """
-    validos = ["0","1","2","3","4","5","6","7","8","9","-","."]
-    malopos = False
-    repite = True
-    num = 0
-    while repite:
-        num = input( mensaje )
-        l = len( num ) #obtener el largo
-        malochar = False
-        malog = False
-        contg = 0
-        contp = 0
-        for i in range( l ):
-            if not num[i] in validos:
-                malochar = True
-            if num[i] == "-":
-                contg = contg+1
-            if num[i] == ".":
-                contp = contp+1
-        malopos = contg == 1 and  num[0] != "-"
-        malog = contg > 1
-        malop = contp > 1
-        if malochar or malog or malopos or malop:
-            repite = True
-            print("La entrada no es válida")
-        else:
-            repite = False
-    numero = float(num)
-    return(numero)
-    #fn_get_valida_num
 
 def fn_mostrar_producto(prod, precio, stock):
     """
@@ -89,13 +57,9 @@ def fn_buscar(lista, nombre):
 #PROGRAMA PRINCIPAL (PP)
 # listas para administrar los productos
 try:
-    lnombre = ["Plumon", "Borrador", "Pizarra"]
-    lprecio = [1280.0, 3500.0, 13500.0]
-    lstock = [20,8, 10]
-
     salir = False
     while not salir:
-        print(f" *** Menú {version.version.version} ***")
+        print(f" *** Menú {version} ***")
         print("[1] Agrega producto")
         print("[2] Listar productos")
         print("[3] Buscar por nombre")
@@ -108,10 +72,13 @@ try:
             nom = input("Nombre del producto: ")    
             lnombre.append( nom ) 
             precio = fn_get_num_valido("Precio del producto: ") #float(input("Precio del producto: ") )
-            lprecio.append( precio ) 
+            lprecio.append( precio )
             canti = fn_get_num_valido("Cantidad del producto: ") #int(input("Cantidad del producto: ") )
-            lstock.append( int(canti)) 
+            lstock.append( int(canti))
+            fn_actualizar_listas
             print(f"Se ha agregado {nom}, con el precio {precio} y el stock {canti}")
+
+            lnombre = lnombre
         #****** Listar producto 
         if (op == "2"):  
             largo = len (lnombre)
